dee7f7b0a35ab08b22537f0ae340300b698d27d8
John.Chen <johnchen5372@gmail.com>
2018-10-28 22:46:15 +0800

    Add apis.py app.py coroweb.py handler.py test.py

--- www/test.py
+++ www/test.py
@@ -4,9 +4,15 @@ from models import User,Blog,Comment
 
 
 def test( loop ):
-    yield from orm.create_pool( loop = loop, user='root', password='123456', db='awesome' )
-    u=User(name='test224',email='test224@test.com',passwd='test',image='about:blank')
-    yield from u.save()
+    yield from orm.create_pool( loop = loop, user='root', password='1q2w3e4R', db='awesome' )
+    u1=User(name='test1',email='test1@test.com',passwd='test1',image='about:blank')
+    yield from u1.save()
+    u2=User(name='test224',email='test2@test.com',passwd='test2',image='about:blank')
+    yield from u2.save()
+    u3=User(name='test224',email='test3@test.com',passwd='test3',image='about:blank')
+    yield from u3.save()
+    u4=User(name='test224',email='test4@test.com',passwd='test4',image='about:blank')
+    yield from u4.save()
 
 
 if __name__ == '__main__':
