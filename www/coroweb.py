f5da31d93c63e1d4fdc9fa359a1e6075a04ce88a
John.Chen <johnchen5372@gmail.com>
2018-10-29 23:07:32 +0800

    用户注册和登录

--- www/coroweb.py
+++ www/coroweb.py
@@ -93,7 +93,7 @@ class RequestHandler(object):
                 ct = request.content_type.lower()
                 if ct.startswith('application/json'):
                     params = await request.json()
-                    if not isinstance(params. dict):
+                    if not isinstance(params, dict):
                         return web.HTTPBadRequest('JSON body must be object.')
                     kw = params
                 elif ct.startswith('application/x-www-form-urlencoded') or ct.startswith('multipart/form-data'):
